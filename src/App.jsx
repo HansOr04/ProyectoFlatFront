00e2bb21b2261120f742dac94b0522ce447d7920
HansOr04 <hansalazar04@gmail.com>
2024-11-19 20:55:53 -0500

    Creacion de la barra de home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from './components/common/Navbar.jsx';
+import Homepage from './components/principal/Homepage.jsx';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Homepage />
       {/* Rest of your app */}
     </div>
   );
