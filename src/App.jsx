91d0e0adb433aefb3266e73110ef1648014a4199
Diana <dianapauly16@hotmail.com>
2024-12-09 18:30:07 -0500

    Cambios realizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import LoginAndSecurity from "./pages/LoginAndSecurity";
+// import LoginAndSecurity from "./pages/LoginAndSecurity";
 // import Notifications from "./pages/Notifications";
 // import LoginRegisterPage from "./pages/LoginRegisterPage";
-import ProfileUpdate from "./pages/ProfileUpdate";
+// import ProfileUpdate from "./pages/ProfileUpdate";
 // import DetailsFlatPage from "./pages/DetailsFlatPage";
 // import CreateFlat from "./pages/CreateFlat";
 // import FavoriteFlat from "./pages/FavoriteFlat";
 // import AllUsers from "./pages/AllUsers";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
     <div>
       {/* <LoginRegisterPage /> */}
-      <ProfileUpdate />
+      {/* <ProfileUpdate /> */}
       {/* <DetailsFlatPage /> */}
       {/* <CreateFlat /> */}
       {/* <FavoriteFlat /> */}
       {/* <AllUsers /> */}
       {/* <LoginAndSecurity /> */}
       {/* <Notifications /> */}
+      <ResetPassword />
     </div>
   );
 }
