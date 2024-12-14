8182d6d8afe65ccfb210f805232ed7c944ae1240
Hans Ortiz <127800719+HansOr04@users.noreply.github.com>
2024-12-14 00:16:39 -0500

    2.0

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,16 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: '/index.html',
-      },
-    },
-    copyPublicDir: true,
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
   },
   server: {
-    port: 3000,
-    host: true,
-    open: true
-  },
-  preview: {
-    port: 8080
-  },
-  base: '/', // Esto es importante para el routing
+    port: 3000
+  }
 })
